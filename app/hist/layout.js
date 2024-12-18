3640207ef6bb1235b0916394b1d4551d51573b29
machage9603 <mikemachage@gmail.com>
2024-12-18 08:58:54 +0300

    hot fix

--- app/hist/layout.js
+++ app/hist/layout.js
@@ -1,6 +1,4 @@
 import { Inter } from 'next/font/google'
-import { Header } from '@/components/header'
-import { Footer } from '@/components/footer'
 import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
